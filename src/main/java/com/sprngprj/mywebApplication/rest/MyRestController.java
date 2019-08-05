ec9af9a0fcbc5b1c2bf3a2dc4300de1ac3019174
Sonam Gadekari <S533490@nwmissouri.edu>
2019-08-05 13:33:37 -0500

    application.properties is changed

--- src/main/java/com/sprngprj/mywebApplication/rest/MyRestController.java
+++ src/main/java/com/sprngprj/mywebApplication/rest/MyRestController.java
@@ -2,11 +2,23 @@ package com.sprngprj.mywebApplication.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+	
+	@Value("${coach.name}")
+	public String coachName;
+	@Value("${team.name}")
+	public String teamName;
+	
+@GetMapping("/teaminfo")
+public String teamFunction() 
+{
+	return coachName+" "+teamName;
+}
 
 @GetMapping("/")
 public String helloFucntion()
